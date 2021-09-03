4e11f303c0b1cd9c2c289203cddd28a0520edbcb
Jerome Onwunalu <jeronwunalu@gmail.com>
2021-09-03 16:29:38 -0500

    Add water production and water injection rate profiles to plot

--- remote_sim_plot_utils.py
+++ remote_sim_plot_utils.py
@@ -4,14 +4,24 @@ def plot_field_results(field_results):
     """Make a sample plot of the oil production rate vs. time (in days)"""
     unit_system = field_results['unit-system']
     field_oil_rates = field_results["oil-prod-rates"]
+    field_water_prod_rates = field_results["water-prod-rates"]
+    field_water_inje_rates = field_results["water-inje-rates"]
+    columns = ['oil-prod-rates', 'water-prod-rates', 'water-inje-rates']
+    legends = ['Oil Prod. Rates', 'Water Prod. Rates', 'Water Inje. Rates']        
     if unit_system.lower() == "field" :
-        oil_unit = "(STB/D)"
+        rate_unit = "(STB/D)"        
     else:
-        oil_unit = "(M3/D)"
+        rate_unit = "(M3/D)"
+        
     time_in_days   = field_results['time-days']
-    plt.plot(time_in_days, field_oil_rates)
+    #plt.plot(time_in_days, field_oil_rates)
+    for column in columns:
+        plt.plot(time_in_days, field_results[column])
+        
+    #plt.plot(time_in_days, field_oil_rates, time_in_days, field_water_prod_rates, )
+    plt.legend(legends)
     plt.grid(True)
     plt.xlabel('Time (days)')
-    plt.ylabel('Field Oil Rate ' + oil_unit)
-    plt.title('Field Oil Production Rate vs. Time (days).')
+    plt.ylabel('Oil, Water Prod. Rates' + rate_unit)
+    plt.title('Field Production and Injection Rates vs. Time')
                
